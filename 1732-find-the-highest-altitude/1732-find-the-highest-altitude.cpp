a98b26821c2550ea3befd943f2cf15876b70394d
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-08-06 13:04:32 +0530

    Time: 0 ms (100%), Space: 10.7 MB (73.09%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int currentAltitude = 0;
+        int highestAltitude = 0;
+        for (int i = 0; i < gain.size(); i++){
+            currentAltitude += gain[i];
+            if ( currentAltitude > highestAltitude){
+                highestAltitude = currentAltitude;
+            }
+        }
+        return highestAltitude;
+    }
+};

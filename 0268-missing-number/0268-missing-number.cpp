c48cd507a79b5154ca6c09c1f296696dbdb9b50e
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-08-14 11:03:44 +0530

    Time: 0 ms (100%), Space: 21.5 MB (99.85%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xorr1 = 0;
+        int xorr2 = 0;
+        for (int i = 0; i <= nums.size(); i++){
+            xorr1 = xorr1 ^ i;
+        }
+        for (int i = 0; i < nums.size(); i++){
+            xorr2 = xorr2 ^ nums[i];
+        }
+        return xorr1 ^ xorr2;
+    }
+};

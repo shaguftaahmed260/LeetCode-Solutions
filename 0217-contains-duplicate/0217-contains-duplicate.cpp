e725727e61f73fa83212fbea25f0db21e93ed748
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-08-15 16:16:56 +0530

    Time: 71 ms (51.4%), Space: 111.3 MB (37.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map <int,int> mp;
+        unordered_map<int,int>mp;
         for (int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for (auto it : mp){
-            if (it.second >= 2){
+            if (mp.find(nums[i]) != mp.end()){
                 return true;
             }
-            else {
-                
-            }
+        mp[nums[i]]++;
         }
         return false;
     }

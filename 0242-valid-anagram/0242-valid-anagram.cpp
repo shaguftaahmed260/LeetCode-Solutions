03d2be6f3aa080c40d3c2d5d8dc576aae4afd6a4
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-08-16 00:44:34 +0530

    Time: 1 ms (54.41%), Space: 9.7 MB (49.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        if (s.length() != t.length()){
             return false;
         }
-        unordered_map<char, int> mp;
-        for (int i = 0; i < s.size(); i++) {
-            mp[s[i]]++;
+        unordered_map <char, int> mpp;
+    for (int i = 0; i < s.size(); i++){
+            mpp[s[i]]++;
         }
-        for (int i = 0; i < t.size(); i++) {
-
-            if (mp.find(t[i]) == mp.end()) {
-                return false;
-            }
-            mp[t[i]]--;
-
-            if (mp[t[i]] < 0) {
-                return false;
-            }
+    for (int i = 0; i < t.size(); i++){
+        if (mpp.find(t[i]) == mpp.end()){
+            return false;  
         }
-        return true;
+        mpp[t[i]]--;
+        if (mpp[t[i]] < 0 ){
+            return false;
+        }
+    }
+    return true;
     }
 };

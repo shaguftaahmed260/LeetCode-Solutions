045c98da1e60a73a51b7270d00f0bcb8abf8f371
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-08-26 21:54:07 +0530

    Time: 0 ms (100%), Space: 15.7 MB (82.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int index = -1;
         for (int i = n - 2; i >= 0; i--){
-            if (nums[i] < nums[i + 1]){
+            if (nums[i] < nums[ i + 1]){
                 index = i;
                 break;
             }
@@ -13,12 +13,13 @@ public:
         reverse(nums.begin(),nums.end());
         return;
     }
-        for (int i = nums.size() - 1; i > index; i--){
+        for (int i = n - 1; i > index; i--){
             if (nums[i] > nums[index]){
-                swap(nums[i] , nums[index]);
+                swap(nums[i],nums[index]);
                 break;
             }
         }
-        reverse (nums.begin() + index + 1, nums.end());
+        reverse(nums.begin() + index + 1, nums.end());
+        
     }
 };

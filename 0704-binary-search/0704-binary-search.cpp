dcc740fa00fa4e44e0d7d9e93029e602b4a48a98
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-09-06 13:48:34 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.06%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,17 +3,17 @@ public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int low = 0;
-        int high = nums.size() - 1;
+        int high = n - 1;
         while (low <= high){
             int mid = low + (high - low) / 2;
             if (nums[mid] == target){
                 return mid;
             }
-            else if (nums[mid] < target){
-                low = mid + 1;
+            else if (nums[mid] > target){
+                high = mid - 1;
             }
             else {
-                high = mid - 1;
+                low = mid + 1;
             }
         }
         return -1;

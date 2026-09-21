b621a4aaeebd72e33e485a539ca27508b92bfe4a
Shagufta Ahmed <shaguftaahmed260@gmail.com>
2026-09-21 21:14:41 +0530

    Time: 0 ms (100%), Space: 263.5 MB (21.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,14 +7,14 @@ public:
             nums[end] = temp;
 
             start++;
-            end --;
+            end --; 
         }
     }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
+        k = k % nums.size();
         reverse(nums, 0 , n - 1);
-        reverse(nums, k , n - 1);
-        reverse(nums, 0 , k - 1);
+        reverse (nums, 0, k -1);
+        reverse (nums, k , n - 1);
     }
 };
